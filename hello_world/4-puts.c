4760003cd0ca4cb2e5a69dbb43a111b6737dee37
fariviriski <7634@holbertonstudents.com>
2023-10-13 12:33:44 -0400

    Fist programm written in c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // using puts to print
+    char* str1 = "Programming is like building a multilingual puzzle\n";
+    puts(str1);0
+ 
+    puts("Programming is like building a multilingual puzzle\n");
+ 
+    return 0;
