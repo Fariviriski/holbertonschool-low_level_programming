761691b948b10df1730dd7ceb11e340aaaac6591
fariviriski <7634@holbertonstudents.com>
2023-10-13 14:31:12 -0400

    using printf with c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main-prints a string of char with prinf
+*
+* Return:always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
