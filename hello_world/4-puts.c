0dd773e9b35410d9a2fae0fd5303240fabef2d83
fariviriski <7634@holbertonstudents.com>
2023-10-13 13:23:47 -0400

    added main comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main entry point
+ *
+ * description: 'Prints a line with certain ext delimited by puts command'
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
