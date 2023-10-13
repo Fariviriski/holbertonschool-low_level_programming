8cba4f50defaf2003f9d210c3492d326c64c4838
fariviriski <7634@holbertonstudents.com>
2023-10-13 13:54:58 -0400

    added description rof return and main

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main - Prints a string of char
+/**
+ * main-Prints a string of char
  *
- * return - 0 if true
+ * Return:Always 0
  */
-
-int main(void){}
+int main(void) {}
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
