cf4860c7b51274a48e87d9c90e541b5c5fb9ea38
fariviriski <7634@holbertonstudents.com>
2023-10-13 13:44:24 -0400

    fixed syntax errors ;

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- * main entry point
+/*
+ * main - Prints a string of char
  *
- * description: 'Prints a line with certain ext delimited by puts command'
+ * return - 0 if true
  */
-int main(void)
+
+int main(void){}
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
