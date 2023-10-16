751028d6a41abbe5a912c916085f92d8cf7b193f
fariviriski <7634@holbertonstudents.com>
2023-10-16 19:07:47 -0400

    putcharfor numbers?

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers base of 10
+ *
+ * Return: 0123456789
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+
+	putchar ('\n');
+	return (0);
+}
