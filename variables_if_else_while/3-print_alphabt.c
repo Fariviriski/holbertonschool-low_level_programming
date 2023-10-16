82f122155de3b6cc9edf7b0d1a7da2373267a4f8
fariviriski <7634@holbertonstudents.com>
2023-10-16 18:32:36 -0400

    using if and && to not print q and e

---
+++ variables_if_else_while/3-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: just the alphabet
+ *
+ */
+
+int main(void)
+{
+        char alphabet;
+
+                for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+					if (alphabet != 'e' && alphabet != 'q');
+                        putchar(alphabet);
+}
+        putchar('\n');
+return (0);
+}
