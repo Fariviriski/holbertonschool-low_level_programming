2fed30ee359a9f7e46756fededaa37dc923db4d4
fariviriski <7634@holbertonstudents.com>
2023-10-16 10:54:24 -0400

    trtying \n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 {
-		printf(""%d"\n is positive");
+		printf("is positive");
 }
 	if (n < 0)
 {
-		printf(""%d"\n is negative");
+		printf("is negative");
 }
 	if (n == 0)
 {
-		printf(""%d"\n is zero")
+		printf("is zero")
 }
 	return (0);
 }
