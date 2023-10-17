fe44e303fbe3e06cc152cd382ee946f63d594d90
fariviriski <7634@holbertonstudents.com>
2023-10-17 12:30:00 -0400

    print reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main - lowercase alphabet in reverse
+ *
+ * Return: reversed alphabet
+ */
+
+int main(void)
+{
+	char rettel;
+
+	for (rettel = 'z'; rettel <= 'a'; rettel++)
+		putchar(rettel);
+
+	putchar('\n');
+
+		return (0);
+}
