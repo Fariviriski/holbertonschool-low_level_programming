57f3edc1045bbc23454ea240a600c1adbd33654a
fariviriski <7634@holbertonstudents.com>
2023-10-17 12:20:07 -0400

    printing numerals using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers base of 10 no print f :(
+ *
+ * Return: 0123456789
+ */
+int main(void)
+{
+		int n;
+
+		for (n = '0'; n <= '9'; n++)
+
+			putchar(n);
+		putchar('\n');
+	return (0);
+}
