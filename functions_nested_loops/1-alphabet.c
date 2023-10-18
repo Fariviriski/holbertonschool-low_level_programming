d84c34708936f2b19dbf5088ea39d8d28a6376fe
fariviriski <7634@holbertonstudents.com>
2023-10-18 10:20:41 -0400

    printing that alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,13 +4,15 @@
  *
  * Return: always 0
  */
-int void print_alphabet(void);
+void print_alphabet(void);
 int main(void)
 {
 	char alpha;
 
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
 			putchar(alpha);
+		}
 		putchar('\n');
 	return (0);
 }
