5f6eddbe6d8c1d4cc6f98ed4574c104c58216d33
fariviriski <7634@holbertonstudents.com>
2023-10-18 11:19:52 -0400

    using 1 to print 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet_x10 - 10 times a-z
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while(i<10);
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha ++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+	}
