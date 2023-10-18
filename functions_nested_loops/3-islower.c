e2ca7f57f46a73c63ffac4dcc58b1785806b0107
fariviriski <7634@holbertonstudents.com>
2023-10-18 13:48:23 -0400

    syntax

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *islower - returns loest char
+ *_islower - returns loest char
  *
  * Return: 0 or 1 if lowercase c
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
+
 		return (1);
-	} else {
+	else
 		return (0);
-	}
+
 }
 
