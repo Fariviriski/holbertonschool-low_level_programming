00e99d1478f235b9b997d4952b1e9b8695972446
fariviriski <7634@holbertonstudents.com>
2023-10-18 12:38:28 -0400

    guillermo is goats

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,18 @@
  *
  * Return: always 0
  */
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
 {
 	char alpha;
 	int i = 0;
 
 	while (i++ <= 9)
 	{
-			for (alpha = 'a'; alpha <= 'z'; alpha ++)
-			{
-				putchar(alpha);
-			}
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			putchar(alpha);
+		}
 		putchar('\n');
-		alpha ++;
+		alpha++;
 	}
 }
-
