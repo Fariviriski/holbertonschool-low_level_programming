8798db2e7acdbc404aff4d60fee937b9d03503a7
fariviriski <7634@holbertonstudents.com>
2023-10-17 20:02:38 -0400

    only used 3 putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers base of 16 no print f :(
+ *
+ * Return: 012345678910
+ */
+	int main(void)
+		{
+				int n;
+				char lett;
+
+				for (n = 0; n < 10; n++)
+					putchar((n % 10) + '0');
+
+				for (lett = 'a'; lett <= 'f'; lett++)
+					putchar(lett);
+
+					putchar('\n');
+	return (0);
+		}
