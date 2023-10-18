04aa2973294a9d5bafe66580c1a40351d4c54d3d
fariviriski <7634@holbertonstudents.com>
2023-10-18 11:57:19 -0400

    try 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *print_alphabet_x10 - 10 times a-z
  *
  * Return: always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void) 
 {
 	int i = 0;
 
