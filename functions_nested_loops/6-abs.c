a870bab7ccc3f510c209cf42764fbd26b3ea25a8
fariviriski <7634@holbertonstudents.com>
2023-10-19 09:58:35 -0400

    prints absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ * @n: character input
+ * _putchar: prints result of abs
+ *
+ * Return: always 0
+ */
+int _abs(int n)	
+{
+	if (n <= 0)
+		return (0);
+	else
+		return(-0);
+}
