f3af28307836e5e9d43110b44a205b53a7e8b67a
fariviriski <7634@holbertonstudents.com>
2023-10-19 09:03:58 -0400

    added n descriptor

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - return 1+ if n>0, 0 if n=0, -1 if n<0
+ *
+ *n: input character
+ *
+ *Return: +1,-1,0
+ */
+int print_sign(int n)
+{
+	char num;
+	{
+		if (n > 0)
+			putchar(num +);
+		return (1);
+	}
+	if else
+	{
+		if (n == 0)
+			putchar(num);
+		return (0);
+	}
+	else
+	{
+		if (n < 0)
+			putchar(num -);
+		return (-1);
+	}
+}
