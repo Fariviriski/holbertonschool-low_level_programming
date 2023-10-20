15463354f2912e172d7d8fde2454edeb4a4ebf1f
fariviriski <7634@holbertonstudents.com>
2023-10-20 12:43:25 -0400

    ay dioj mio

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,13 @@
 /**
  * print_most_characters - prints 0-9 excluding 2,4
  */
- void print_most_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
-	for (num = 0; num <= 9;num++)
+
+	for (num = 0; num <= 9; num++)
 	{
-		if(num != 2 && num != 4)
+		if (num != 2 && num != 4)
 			_putchar((num % 10) + '0');
 	}
 	_putchar('\n');
