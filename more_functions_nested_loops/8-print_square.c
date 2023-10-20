cf4fd932226e3da90851590d143411b7191479bc
fariviriski <7634@holbertonstudents.com>
2023-10-20 14:46:09 -0400

    no quiero mas :(

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - diag line
+ * @size: input char
+ */
+void print_square(int size)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < size; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
