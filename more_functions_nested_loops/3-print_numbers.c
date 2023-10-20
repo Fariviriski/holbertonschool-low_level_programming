1e5fbae6fc2d8e1755eae391b48819fe6b37e10f
fariviriski <7634@holbertonstudents.com>
2023-10-20 10:36:33 -0400

    first commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - prints a character
+ * _purchar - print funct
+ * @c: input character
+ */
+void print_numbers(void)
+{
+	int c;
+		for (c >= '0'; c<= '9'; c++)
+			_putchar('c');
+			_putchar('\n');
+}
