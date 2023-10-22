bad7bf2ebc420df2a65082e6c42e9aaa868bd129
fariviriski <7634@holbertonstudents.com>
2023-10-22 13:16:25 -0400

    syntax

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d\n ", n);
 		if (n % 3 == 0 && n % 5 != 0)
-			printf("Fizz \n");
+			printf("Fizz ");
 		if (n % 5 == 0 && n % 3 != 0)
-			printf("Buzz \n");
+			printf("Buzz ");
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("Fizz Buzz \n");
+			printf("Fizz Buzz ");
 	}
 	return (0);
 }
