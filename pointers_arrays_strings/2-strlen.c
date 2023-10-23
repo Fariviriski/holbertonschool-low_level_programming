ff63887e0e21786d458f3c4c2270b754a896a6d4
fariviriski <7634@holbertonstudents.com>
2023-10-23 10:46:45 -0400

    first commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - prints string lenght
+ * @s: string of characters to be deferenced
+ * Return: lenght of pointed string
+ */
+
+int _strlen(char *s)
+{
+	return (*s);
+}
