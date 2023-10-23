5dff2a4c7a9029da9e3e1714c46314ccfd040dc0
fariviriski <7634@holbertonstudents.com>
2023-10-23 14:18:30 -0400

    jesusssss

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  *_puts - printer
- *_putchar: used for printing
  * @str: string to be printed
  */
 
 void _puts(char *str)
 {
-int count = 0;
+	int count = 0;
 
 	while(str[count] != '\0')
 	{
-	_putchar(str[count]);
-	count++;
+		_putchar(str[count]);
+		count++;
 	}
 	_putchar('\n');
 }
