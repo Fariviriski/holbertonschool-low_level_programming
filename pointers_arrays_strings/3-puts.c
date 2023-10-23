8471c8a2044ad11095d157859db0b47478e599ea
fariviriski <7634@holbertonstudents.com>
2023-10-23 13:55:34 -0400

    aiuda...

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,10 +9,10 @@ void _puts(char *str)
 {
 int count = 0;
 
-	while(str[count] != '\0')
+	while(*str[count] != '\0')
 	{
-	_putchar(str[count]);
-	count++
+	_putchar(*str[count]);
+	count++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
