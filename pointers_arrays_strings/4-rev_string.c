24e770fd319d1dfc37b8043a6cef49ee21b140a7
fariviriski <7634@holbertonstudents.com>
2023-10-24 09:14:45 -0400

    first try

---
+++ pointers_arrays_strings/4-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	lenght = _strlen(s) -1;
+	for (i = 0; i < _strlen(s)/2; i++0
+			{
+			temp = s[i];
+			s[i] = s[len];
+			s[len--]=temp;
+			}
+			_putchar(s)
+			}
+
