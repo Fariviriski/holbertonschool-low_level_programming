a6a59d5f5f154ef1cbaf5f833b1502fe0f21fb69
fariviriski <7634@holbertonstudents.com>
2023-10-24 09:33:30 -0400

    4 brooo

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,14 @@
 
 void rev_string(char *s)
 {
-	int i, len, temp;
+	int len = 0, index;
 
-	len = _strlen(s) -1;
-	for (i = 0; i < _strlen(s)/2; i++)
-			{
-			temp = s[i];
-			s[i] = s[len];
-			s[len--]=temp;
-			}
-			_putchar(*s);
+	while (s[index++])
+		len++;
+
+	for (indez = len -1; index >= 0; index --)
+		_putchar(s[index]);
+
+	_putchar(('\n');
 			}
 
