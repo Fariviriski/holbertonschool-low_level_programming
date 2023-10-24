c98a31b9fceae604948e312a37377affbc6edc71
fariviriski <7634@holbertonstudents.com>
2023-10-24 10:36:29 -0400

    new code

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,23 @@
  */
 void rev_string(char *s)
 {
-	int i, len;
-	len = _strlen(s);
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
 
 	for (i = 0; i < len / 2; i++)
 	{
-		int temp;
-
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len -i - 1] = temp;
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
