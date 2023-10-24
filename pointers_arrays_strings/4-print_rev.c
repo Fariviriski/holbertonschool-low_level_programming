a7dbcbeb076c798ddd54e1a560efcbf389aa601b
fariviriski <7634@holbertonstudents.com>
2023-10-24 10:12:57 -0400

    seventh, yup

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,16 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
+	int n;
 
-	while (s[index++])
+	while (s[len] != '\0')
+	{
 		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	}
+	for(n = len -1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
 	_putchar('\n');
-			}
-
+}
