a8a50e09637a0731356585fb3b93d1966f88c23f
fariviriski <7634@holbertonstudents.com>
2023-10-24 13:25:29 -0400

    new loop

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,10 +8,13 @@
 
 void print_array(int *a, int n)
 {
-	int len = sizeof(a);
-
-	for (n = 0; n < len; n++)
+	nt i;
+	for(i = 0; i < n; i++)
 	{
-		printf("%d, ", a[n]);
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
+	printf("\n");
 }
