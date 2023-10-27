bd6e6c0a12bacf7bba1bb956ff7d1f915a22c1bb
fariviriski <7634@holbertonstudents.com>
2023-10-27 13:51:31 -0400

    syntax

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * _strcmp - compares two strings
+ * @index: index of a string
+ * Return: 0 if equal, -1 is=f lesser, 1 if greater
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	while (index[s1] == index[s2])
+	{
+		return (0);
+	}
+
+	if (index[s1] < index[s2])
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
