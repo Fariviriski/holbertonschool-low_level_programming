13f9034dc095497bceb7a29dc8cdc72e5a750209
fariviriski <7634@holbertonstudents.com>
2023-10-27 14:04:59 -0400

    syntax4

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * _strcmp - compares two strings
- * @index: index of a string
  * @s1: first string
  * @s2: second string
  * Return: 0 if equal, -1 if lesser, 1 if greater
