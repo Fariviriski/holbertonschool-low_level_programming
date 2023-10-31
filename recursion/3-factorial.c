ae9536a2a08c56e7157553567879df6daf90bec3
fariviriski <7634@holbertonstudents.com>
2023-10-31 12:53:41 -0400

    first commit

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - prints the factorial of a num
+ * @n: number to be factorized
+ * Return: 0 if natural num -1 if error
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n > 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
