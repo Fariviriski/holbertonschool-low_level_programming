8ad74275d6358b122e03a328fbf38a1e92d56bc7
fariviriski <7634@holbertonstudents.com>
2023-11-01 21:04:30 -0400

    syntax

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,7 +1,12 @@
 #include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
 /**
- *
+ * _sqrt_recursion - primary function to find square root
+ * @n: number to be checked
+ * Return: call to recursion
  */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -9,11 +14,18 @@ int _sqrt_recursion(int n)
 	return (actual_sqrt_recursion(n, 0));
 }
 
+ /**
+ * actual_sqrt_recursion - mathematical equation to fnd suqare root
+ * @n: number to be cheked
+ * @i: integer number
+ * Return: natural square root of n
+ */
+
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i +1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
