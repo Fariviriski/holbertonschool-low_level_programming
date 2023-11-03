7673ec9bd5c6cc1b1ab3fbe9554b8e67ac3a6794
fariviriski <7634@holbertonstudents.com>
2023-11-03 11:04:46 -0400

    syntax

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *
+ * main - multiplies number
+ * @argc: counts
+ * @argv: vector
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	int n, b = 0;
 
-	if (argc !=3)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		b = 1;
@@ -16,7 +19,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		n = atoi(argv[1]) * atoi(argv[2]);
-		printf ("%i\n", n);
+		printf("%i\n", n);
 	}
 	return (b);
 }
