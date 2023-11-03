2680f946738d455cb1ca6a9b6ccd8190d4b8ad88
fariviriski <7634@holbertonstudents.com>
2023-11-03 10:58:43 -0400

    first commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int n, b = 0;
+
+	if (argc !=3)
+	{
+		printf("%s\n", "error");
+		b = 1;
+	}
+	else
+	{
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf ("%i\n", n);
+	}
+	return (b);
+}
