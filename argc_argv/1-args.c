351e605be5268e80cd4dcdf81af73cf2fb46d21e
fariviriski <7634@holbertonstudents.com>
2023-11-03 10:29:47 -0400

    first commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ * @argc: count of arguments
+ * @argv: unused vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
