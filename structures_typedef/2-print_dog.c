7f829a1a9ff9c75945cddf4d94913f88eb8c18a0
fariviriski <7634@holbertonstudents.com>
2023-11-16 18:36:36 -0400

    first'

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - printing that doggy
+ * @d: a pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(Null)";
+
+	if (d->owner == NULL)
+		d->owner = "(Null)";
+
+	printf("name: %s\nage %f\nowner: %s\n", d->name, d->age, d->owner);
+}
