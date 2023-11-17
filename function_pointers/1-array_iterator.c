d3053166341e2977cbb17196ddffab68e6f8cf05
fariviriski <7634@holbertonstudents.com>
2023-11-16 21:34:30 -0400

    first

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a func given as parameter
+ * @array: array
+ * @size: size of array
+ * @action: pointer to func
+ */
+
+void array_iterator(int *array, size_t size, void (*actiion)(int))
+{
+
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
