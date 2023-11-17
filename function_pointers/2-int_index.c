6361f20cc16093cf9728842bec09cdff4649c36c
fariviriski <7634@holbertonstudents.com>
2023-11-16 21:59:58 -0400

    trying for an if

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,14 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i] != 0))
-				return (i);
-		}
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
+	for (i = 0; i < size; Index++)
+	{
+		if(cmp(array[index]) != 0)
+			return (i);
 	}
 	return (-1);
 }
