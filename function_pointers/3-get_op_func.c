ad26aec41117fee73611f55a065ac69583fb6c7c
fariviriski <7634@holbertonstudents.com>
2023-11-16 22:39:27 -0400

    i have a headache

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - function selector for operator
+ * @s: selector
+ * Return: pointer to correct func
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
+
+	return (ops[i].f);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stlib.h>
+#include "3-calc.c"
+
+/**
+ * main - prints result of operation
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: alvays 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int n1, n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	op = qrgv[2];
+	n2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (*op == '/' && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (*op == '%' && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(n1, n2));
+
+	return (0);
+}
