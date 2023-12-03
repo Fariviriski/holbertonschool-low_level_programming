91a3f9a5553071be7b465d34a746bc1c07fdffe8
fariviriski <7634@holbertonstudents.com>
2023-12-03 13:21:05 -0400

    tests

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,19 +10,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t counter = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		counter++;
 		h = h->next;
 	}
-
-	return (nodes);
+	return (counter);
 }
