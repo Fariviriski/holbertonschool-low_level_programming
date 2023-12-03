74c58bcdd98b432892f9e046b4c6fb439b7fa63d
fariviriski <7634@holbertonstudents.com>
2023-12-03 13:09:46 -0400

    fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,20 +1,27 @@
 #include "lists.h"
 
 /**
- * print_list - prints elements of a list
- * @h: list
+ * print_list - print all the elements of a list_t list
+ * @h: list_t list
  *
- * Return: nodes in h
+ * Return: the number of nodes in h
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nodes = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (i);
+
+	return (nodes);
 }
