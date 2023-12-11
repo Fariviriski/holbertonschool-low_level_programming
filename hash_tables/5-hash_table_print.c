7f598da2443e12c0538920087c3bd877f0de8a41
fariviriski <7634@holbertonstudents.com>
2023-12-10 22:59:10 -0400

    syntax

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -15,16 +15,16 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 	printf("{");
 
-	for (i = 0; i < ht-<size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NU::)
+		if (ht->array[i] != NULL)
 		{
 			if (comma_flag == 1)
 				printf(", ");
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				printf("'%s': '%s"', node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 				if (node != NULL)
 					printf(", ");
