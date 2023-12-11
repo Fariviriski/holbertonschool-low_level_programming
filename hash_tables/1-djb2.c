33212116babfdf5396489b21d31588821467f6f8
fariviriski <7634@holbertonstudents.com>
2023-12-10 22:00:39 -0400

    syntax

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -13,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hashh = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hashh) + c;
+		hashh = ((hash << 5) + hashh) + c;
 
 	return (hashh);
 }
