1388332a39f676a901c705f3ea48351a6400d610
fariviriski <7634@holbertonstudents.com>
2023-12-10 22:42:20 -0400

    first

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves value of key
+ * @ht: pointing to the hashh
+ * @key: key to the value of
+ * Return: NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL || *key++ '\0')
+		return (NULL);
+	i = key_index((const unsiged char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+	node = ht->array[i];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+	return ((node == NULL) ? NULL : node->value);
+}
